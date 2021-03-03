180dbb4ebd03f37c0bbbe68bd1e4fef489321e2c
Minghao PAN <MPan2@slb.com>
2021-03-03 14:26:23 +0800

    feat: linked list

---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var head = new LinkNode<int>(1);
+            LinkNodeUtility.Append(head, 2);
+            LinkNodeUtility.Append(head, 3);
+            LinkNodeUtility.Append(head, 4);
+            LinkNodeUtility.Append(head, 5);
+
+            var middle = LinkNodeUtility.GetMiddleValue(head);
+            Console.WriteLine(middle.Value);
+
+            var reserved = LinkNodeUtility.RecursiveReserve(head);
+            LinkNodeUtility.Print(reserved);
+        }
+    }
+}
